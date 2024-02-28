cc5af4b576f011f90364178fa0d59bf1fcdbe450
PerlaGCastillo <contactoperlagodinez@gmail.com>
2024-02-28 16:05:28 -0600

    professor auth doesnt work - students ok

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,32 +1,58 @@
-import pool from "../database/keys"
+import pool from "../database/keys";
 
-const authentication = {}
+const authentication = {};
 
 authentication.signUp = async (req, res) => {
-  const {nombre, email, password, role} = req.body
-  if (role=='professor'){
+  const { nombre, email, password, role } = req.body;
+  if (role=='professor') {
     try {
-        await pool.query('INSERT INTO professor (p_nombre,p_email,p_password) VALUES ($1, $2, $3)', [nombre, email, password]);
-        res.status(200).json({
-            message: 'Profesor registrado con éxito',
-            professor: {nombre, email, password}
-        })
-    } catch(error) {
-        if (error.constraint == "professor_p_email_key") {
-            res.status(500).json({
-                message: 'Correo electrónico ya registrado, por favor, ingresa un correo válido',
-                error
-            })
-        } else {
-            res.status(500).json({
-                message: 'ha ocurrido un error', 
-                error
-            })
-        }
+      await pool.query(
+        "INSERT INTO professor(p_nombre,p_email,p_password) VALUES ($1, $2, $3)",
+        [nombre, email, password]
+      );
+      res.status(200).json({
+        message: "Profesor registrado con éxito",
+        professor: { nombre, email, password },
+      });
+    } catch (error) {
+      if (error.constraint == "professor_p_email_key") {
+        res.status(500).json({
+          message:
+            "Correo electrónico ya registrado, por favor, ingresa un correo válido",
+          error,
+        });
+      } else {
+        res.status(500).json({
+          message: "ha ocurrido un error",
+          error,
+        });
+      }
     }
-  } else{
-    res.send('student')
+  } else {
+    try {
+      await pool.query(
+        "INSERT INTO student(s_nombre,s_email,s_password) VALUES($1, $2, $3)",
+        [nombre, email, password]
+      );
+      res.status(200).json({
+        message: "Estudiante registrado con éxito",
+        student: { nombre, email, password },
+      });
+    } catch (error) {
+      if (error.constraint == "student_s_email_key") {
+        res.status(500).json({
+          message:
+            "Correo electrónico ya registrado, por favor, ingresa un correo válido",
+          error,
+        });
+      } else {
+        res.status(500).json({
+          message: "ha ocurrido un error",
+          error,
+        });
+      }
+    }
   }
-}
+};
 
-module.exports = authentication
+module.exports = authentication;
