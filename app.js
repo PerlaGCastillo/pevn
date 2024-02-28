31654bdc046a0e757b029fdf18faad2baac6a3df
PerlaGCastillo <contactoperlagodinez@gmail.com>
2024-02-28 13:19:51 -0600

    db created

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+import fileUpload from 'express-fileupload'
+import historyApiFallback from 'connect-history-api-fallback'
+import path from 'path'
+
 const app = express()
 
-
 //middlewares
-
-
+app.use(morgan('tiny'))
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(fileUpload({useTempFiles: true}))
+app.use(historyApiFallback())
+app.use(express.static(path.join(__dirname, 'public')))
+// console.log(__dirname)
 
 // routes
 app.get('/', (req, res) =>{
     res.send('hola mundo')
 
 })
-
 
 // settings 
 app.set('port', process.env.PORT || 3000);
---
+++ database/db.sql
@@ -0,0 +1,48 @@
+CREATE DATABASE pevn;
+
+-- \c pevn
+
+CREATE TABLE professor (
+    id_p SERIAL PRIMARY KEY,
+    p_nombre TEXT NOT NULL, 
+    p_email TEXT NOT NULL, 
+    p_password TEXT NOT NULL
+);
+
+CREATE TABLE student (
+    id_s SERIAL PRIMARY KEY,
+    s_nombre TEXT NOT NULL, 
+    s_email TEXT NOT NULL, 
+    s_password TEXT NOT NULL
+);
+
+CREATE TABLE course (
+    id_c SERIAL PRIMARY KEY,
+    id_p INTEGER REFERENCES professor(id_p), 
+    c_name TEXT NOT NULL, 
+    c_description TEXT NOT NULL
+);
+
+
+CREATE TABLE stucentvscourse(
+    s_id INTEGER NOT NULL REFERENCES student(id_s),
+    c_id INTEGER NOT NULL REFERENCES course(id_c)
+);
+
+CREATE TABLE assignment(
+    id_a SERIAL PRIMARY KEY,
+    c_ic INTEGER NOT NULL REFERENCES course(id_c),
+    a_name TEXT NOT NULL, 
+    a_description TEXT,
+    a_file TEXT NOT NULL
+);
+
+create TABLE delivery(
+    id_d SERIAL PRIMARY KEY,
+    a_id INTEGER REFERENCES assignment(id_a),
+    s_id INTEGER REFERENCES student(id_s),
+    d_file TEXT NOT NULL,
+    d_filename TEXT
+);
+
+-- \dt
