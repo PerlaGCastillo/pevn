9bd2117fa723ffbfcbf15c89cec08116f6136a82
PerlaGCastillo <contactoperlagodinez@gmail.com>
2024-02-28 17:57:55 -0600

    signup signin config

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,31 +1,31 @@
-import pool from "../database/keys";
+import pool from "../database/keys"
 
-const authentication = {};
+const authentication = {}
 
 authentication.signUp = async (req, res) => {
-  const { nombre, email, password, role } = req.body;
-  if (role=='professor') {
+  const { nombre, email, password, role } = req.body
+  if (role == "professor") {
     try {
       await pool.query(
-        "INSERT INTO professor(p_nombre,p_email,p_password) VALUES ($1, $2, $3)",
+        "INSERT INTO professor(p_nombre, p_email, p_password) VALUES ($1, $2, $3)",
         [nombre, email, password]
-      );
+      )
       res.status(200).json({
         message: "Profesor registrado con éxito",
         professor: { nombre, email, password },
-      });
+      })
     } catch (error) {
       if (error.constraint == "professor_p_email_key") {
         res.status(500).json({
           message:
             "Correo electrónico ya registrado, por favor, ingresa un correo válido",
-          error,
-        });
+          error
+        })
       } else {
         res.status(500).json({
           message: "ha ocurrido un error",
-          error,
-        });
+          error
+        })
       }
     }
   } else {
@@ -33,26 +33,81 @@ authentication.signUp = async (req, res) => {
       await pool.query(
         "INSERT INTO student(s_nombre,s_email,s_password) VALUES($1, $2, $3)",
         [nombre, email, password]
-      );
+      )
       res.status(200).json({
         message: "Estudiante registrado con éxito",
         student: { nombre, email, password },
-      });
+      })
     } catch (error) {
       if (error.constraint == "student_s_email_key") {
         res.status(500).json({
           message:
             "Correo electrónico ya registrado, por favor, ingresa un correo válido",
-          error,
-        });
+          error
+        })
       } else {
         res.status(500).json({
           message: "ha ocurrido un error",
-          error,
-        });
+          error
+        })
       }
     }
   }
-};
+}
+
+authentication.signIn = async (req, res) =>{
+  const{email, password, role} = req.body
+  if(role=="professor"){
+    try {
+      const professor = await (await pool.query('SELECT * FROM professor WHERE p_email=$1 AND p_password=$2', [email,password])).rows
+      if (professor.length>0) {
+        res.status(200).json({
+          id: professor[0].id_p,
+          name: professor[0].p_name,
+          email: professor[0].p_email,
+          role: 'professor'
+        })
+      } else {
+        res.status(200).json({
+          message:'Profesor no existe',
+          NotFound: true
+        })
+      }
+    } catch (error) {
+      res.status(500).json({
+        message:'Error',
+        error
+      })
+      
+    }
+
+  }else{
+    try {
+      const student = await (await pool.query('SELECT * FROM student WHERE s_email=$1 AND s_password=$2', [email,password])).rows
+      if (student.length>0) {
+        res.status(200).json({
+          id: student[0].id_s,
+          name: student[0].s_name,
+          email: student[0].s_email,
+          role: 'student'
+        })
+      } else {
+        res.status(200).json({
+          message:'Estudiante no existe',
+          NotFound: true
+        })
+      }
+    } catch (error) {
+      res.status(500).json({
+        message:'Error',
+        error
+      })
+      
+    }
+
+  }
+  
+
+}
 
 module.exports = authentication;
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,5 +4,6 @@ import authentication from '../controllers/auth'
 const router = express.Router()
 
 router.post("/signup", authentication.signUp)
+router.post("/signin", authentication.signIn)
 
 module.exports=router
