43ba7857a975120d49fe8cec2f064e44a30d7c77
PerlaGCastillo <contactoperlagodinez@gmail.com>
2024-02-29 14:58:58 -0600

    babel parse error wip

--- controllers/lib/cloudinary.js
+++ controllers/lib/cloudinary.js
@@ -1,10 +1,20 @@
-import cloudinary from "cloudinary"
+import {v2 as cloudinary} from "cloudinary"
 
-cloudinary({
-  cloud_name: "dcbcf1jtk",
-  api_key: "449196468941497",
-  api_secret: "a1xpaCZlma3d-wzF_Dqk0sAVsso",
+const cloudinary = require('cloudinary'.v2)
+
+
+ cloudinary.config({
+   cloud_name: dcbcf1jtk,
+   api_key: 449196468941497,
+   api_secret: a1xpaCZlma3d-wzF_Dqk0sAVsso,
 })
+  
+// cloudinary.config({
+//   cloud_name:process.env.CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_KEY,
+//   api_secret:process.env.CLOUDINARY_SECRET
+// });
+
 
 module.exports = async (file) => {
   try {
